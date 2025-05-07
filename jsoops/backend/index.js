b63c63853fede5152b0acd11eaa9d4b0c91efd17
premkumartech <premkumartech123@gmail.com>
2025-05-07 22:34:20 +0530

    #02 handling post requests

--- jsoops/backend/index.js
+++ jsoops/backend/index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 app.get("/register", (req, res) => {
     let {user, password } = req.query;
     res.send(`standard GET response. welcome ${ user }`);
 });
 
 app.post("/register", (req, res) => {
-    res.send("standard POST response");
+    let {user, password } = req.body;
+    res.send(`standard POST response. Welcome ${ user }`);
 });
 
 app.listen(port, () => {
