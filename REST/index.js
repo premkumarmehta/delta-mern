bfd14e1a5131c121d267788b96eeb31983aeeaa6
premkumartech <premkumartech123@gmail.com>
2025-05-09 23:02:13 +0530

    #07 show route : GET/posts/:id

--- REST/index.js
+++ REST/index.js
@@ -12,14 +12,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [ // #04 index route
     {
+        id : "1a",
         username : "premkumarmehta",
         content : "I love coding"
     },
     {
+        id : "2b",
         username : "premkumartech",
         content : "Hard work is important to achive success"
     },
     {
+        id : "3c",
         username : "prem_.mehta",
         content : "I got selected for my 1st intership!"
     },
@@ -40,9 +43,22 @@ app.get("/posts/new", (req, res) => {
 
 app.post("/posts", (req, res) => {
     // console.log(req.body);
+
     let { username, content } = req.body;
     posts.push({username, content});
-    res.send("post request working");
+
+    // res.send("post request working");
+    res.redirect("/posts"); // #6 redirect: after submiting post directly, redirect to the /post page.
+});
+
+// #07 show route
+
+app.get("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    // console.log(post);
+    // res.send("request working");
+    res.render("show.ejs", { post });
 });
 
 // app.get("/", (req, res) => {
