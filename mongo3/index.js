2032b948d58e64f699de219e0ab4954138699ca1
premkumartech <premkumartech123@gmail.com>
2025-05-19 17:07:42 +0530

    index route (GET /chats)

--- mongo3/index.js
+++ mongo3/index.js
@@ -7,6 +7,8 @@ const Chat = require("./models/chat.js");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.static(path.join(__dirname, "public")));
+
 main().then(() => {
     console.log("connection successful");
 })
@@ -16,16 +18,25 @@ async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
 
-let Chat1 = new Chat({
-    from: "prem",
-    to: "prince",
-    msg: "send me your exam sheets",
-    created_at: new Date() 
-});
+// let Chat1 = new Chat({
+//     from: "prem",
+//     to: "prince",
+//     msg: "send me your exam sheets",
+//     created_at: new Date() 
+// });
 
-Chat1.save().then((res) => {
-    console.log(res);
-})
+// Chat1.save().then((res) => {
+//     console.log(res);
+// })
+
+// Index Route
+
+app.get("/chats", async (req, res) => {
+    let chats = await Chat.find();
+    console.log(chats);
+    // res.send("working...");
+    res.render("index.ejs", { chats });
+});
 
 app.get("/", (req, res) => {
     res.send("root is working");
