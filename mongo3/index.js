8aedc628d548ce6f40fdc1fa45546086f8830e42
premkumartech <premkumartech123@gmail.com>
2025-05-22 11:52:52 +0530

    #10 destroy route and completed

--- mongo3/index.js
+++ mongo3/index.js
@@ -89,6 +89,15 @@ app.put("/chats/:id", async (req, res) => {
     res.redirect("/chats");
 })
 
+ // destroy route
+
+app.delete("/chats/:id", async (req, res) => {
+    let { id } = req.params;
+    let deletedChat = await Chat.findByIdAndDelete(id);
+    console.log(deletedChat);
+    res.redirect("/chats");
+})
+
 app.get("/", (req, res) => {
     res.send("root is working");
 });
