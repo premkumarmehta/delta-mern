3b3bab258c0efdf9a72c9a8b0c9b3ecfecf6dd01
premkumartech <premkumartech123@gmail.com>
2025-05-22 11:19:52 +0530

    #08 edit and #09 update route

--- mongo3/index.js
+++ mongo3/index.js
@@ -3,12 +3,15 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+const methodOverride = require("method-override");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended: true}));
+
+app.use(methodOverride("_method"));
 
 main().then(() => {
     console.log("connection successful");
@@ -67,6 +70,24 @@ app.post("/chats", (req, res) => {
     res.redirect("/chats");
 });
 
+// Edit route
+
+app.get("/chats/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs", { chat });
+})
+
+// update route
+
+app.put("/chats/:id", async (req, res) => {
+    let { id } = req.params;
+    let { msg: newMsg } = req.body;
+    let updateChat = await Chat.findByIdAndUpdate(id, {msg: newMsg}, {runValidators: true, new: true});
+
+    console.log(updateChat);
+    res.redirect("/chats");
+})
 
 app.get("/", (req, res) => {
     res.send("root is working");
